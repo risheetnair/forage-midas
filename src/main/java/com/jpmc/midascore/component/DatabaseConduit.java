fd6c65df8dcd1cd63f9c67b1516e2cf50245cde5
Risheet Nair <risheetnair@outlook.com>
2025-12-18 16:59:59 -0500

    integrated SQL database using H2 and JPA

--- src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
+++ src/main/java/com/jpmc/midascore/component/DatabaseConduit.java
@@ -1,19 +1,63 @@
 package com.jpmc.midascore.component;
 
+import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Transaction;
+import com.jpmc.midascore.repository.TransactionRecordRepository;
 import com.jpmc.midascore.repository.UserRepository;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
 
 @Component
 public class DatabaseConduit {
     private final UserRepository userRepository;
+    private final TransactionRecordRepository transactionRecordRepository;
 
-    public DatabaseConduit(UserRepository userRepository) {
+    public DatabaseConduit(UserRepository userRepository,
+            TransactionRecordRepository transactionRecordRepository) {
         this.userRepository = userRepository;
+        this.transactionRecordRepository = transactionRecordRepository;
+    }
+
+    @Transactional
+    public void process(Transaction tx) {
+        Optional<UserRecord> senderOpt = userRepository.findById(tx.getSenderId());
+        if (senderOpt.isEmpty()) {
+            return;
+        }
+
+        Optional<UserRecord> recipientOpt = userRepository.findById(tx.getRecipientId());
+        if (recipientOpt.isEmpty()) {
+            return;
+        }
+
+        UserRecord sender = senderOpt.get();
+        UserRecord recipient = recipientOpt.get();
+
+        float amount = tx.getAmount();
+
+        if (sender.getBalance() < amount) {
+            return;
+        }
+
+        sender.setBalance(sender.getBalance() - amount);
+        recipient.setBalance(recipient.getBalance() + amount);
+
+        userRepository.save(sender);
+        userRepository.save(recipient);
+
+        TransactionRecord record = new TransactionRecord(sender, recipient, amount);
+        transactionRecordRepository.save(record);
     }
 
     public void save(UserRecord userRecord) {
         userRepository.save(userRecord);
+    }
+
+    public float getBalanceByName(String name) {
+        UserRecord user = userRepository.findByName(name);
+        return user.getBalance();
     }
 
 }
--- src/main/java/com/jpmc/midascore/kafka/TransactionListener.java
+++ src/main/java/com/jpmc/midascore/kafka/TransactionListener.java
@@ -2,13 +2,21 @@ package com.jpmc.midascore.kafka;
 
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+
+import com.jpmc.midascore.component.DatabaseConduit;
 import com.jpmc.midascore.foundation.Transaction;
 
 @Component
 public class TransactionListener {
 
+    private final DatabaseConduit databaseConduit;
+
+    public TransactionListener(DatabaseConduit databaseConduit) {
+        this.databaseConduit = databaseConduit;
+    }
+
     @KafkaListener(topics = "${general.kafka-topic}", groupId = "midas-core")
     public void listen(Transaction transaction) {
-        System.out.println(transaction.getAmount());
+        databaseConduit.process(transaction);
     }
 }
